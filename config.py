5aea50f4c500cddb986c6b15eaf768129b866d8b
Kichuprobotzzz <111451450+Kichuprobotzzz@users.noreply.github.com>
2024-10-29 20:54:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = os.environ.get("API_ID", "28714959")
+    API_HASH = os.environ.get("API_HASH", "c0b9797634090ee3f4c1c56db6c051a7")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7580186761:AAFGUsYWhrirgDgsE5QosbapbdG97Rn-HH0") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://jiosaavn:jiosaavn@cluster0.ouhhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5398056049 1905251964').split()]
 
 
 class temp(object): 
